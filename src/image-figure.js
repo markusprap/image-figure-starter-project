38c5046ffdbb1a25ebc9a19beeedb82f7bf74fb3
Markus Prap Kurniawan <prapkurniawanmarkus@gmail.com>
2025-05-01 19:33:50 +0700

    update

---
+++ src/image-figure.js
@@ -0,0 +1,18 @@
+class imageFigure extends HTMLElement {
+    constructor() {
+        super();
+
+        this.render();
+    }
+
+    render(){
+        this.innerHTML = `
+            <figure>
+                <img src="src/logo-g.jpg" alt="Dicoding Logo g" width="200">
+                <figcaption>Huruf g dalam logo Dicoding</figcaption>
+            </figure>
+        `;
+    }
+}
+
+customElements.define('image-figure',imageFigure);
